b8bed9c31eaf90f14632bad7fde40ea466400df8
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-18 08:31:29 -0300

    Initial commit

---
+++ src/main/java/com/example/ProvaSaep/ProvaSaepApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProvaSaep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSaepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSaepApplication.class, args);
+	}
+
+}
